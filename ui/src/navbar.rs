ca0e020072901f27df1aef135595969fc22656a0
ReadyPlayerEmma <emma@readyplayeremma.com>
2025-09-20 22:19:18 -0600

    Enable desktop feature by default and inline navbar CSS in release

--- ui/src/navbar.rs
+++ ui/src/navbar.rs
@@ -1,11 +1,18 @@
 use dioxus::prelude::*;
 
 const NAVBAR_CSS: Asset = asset!("/assets/styling/navbar.css");
+const NAVBAR_CSS_INLINE: &str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/assets/styling/navbar.css"
+));
 
 #[component]
 pub fn Navbar(children: Element) -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: NAVBAR_CSS }
+        if cfg!(all(not(debug_assertions), not(target_arch = "wasm32"))) {
+            document::Style { "{NAVBAR_CSS_INLINE}" }
+        }
 
         div {
             id: "navbar",
